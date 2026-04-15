954686c0654dc4b4e8ea340532437a9a62a32412
zookat <zotova.ea@gmail.com>
2026-04-15 15:56:05 +0200

    Add toggle to pistas

--- script.js
+++ script.js
@@ -2,21 +2,23 @@ const inicio = document.getElementById("pantalla-inicio");
 const reto1 = document.getElementById("reto1");
 const succses = document.getElementById("pantalla-final-exito");
 const wasted = document.getElementById("pantalla-final-wasted");
+const togglePista1 = document.getElementById("togglePista1");
+const pista1 = document.getElementById("pista1");
 
 function testRetoUno() {
-   if (machine.getValue() == 555) {
-      succses.removeAttribute("hidden");
-   } else {
-      wasted.removeAttribute("hidden");
-      console.log("machine.getValue()");
-   }
-   reto1.setAttribute("hidden", true);
+  if (machine.getValue() == 555) {
+    succses.removeAttribute("hidden");
+  } else {
+    wasted.removeAttribute("hidden");
+    console.log("machine.getValue()");
+  }
+  reto1.setAttribute("hidden", true);
 }
 
 const btn = inicio.querySelector("button");
 btn.addEventListener("click", () => {
-   inicio.setAttribute("hidden", true);
-   reto1.removeAttribute("hidden");
+  inicio.setAttribute("hidden", true);
+  reto1.removeAttribute("hidden");
 });
 
 const btnReto1 = reto1.querySelector("button");
@@ -24,12 +26,20 @@ btnReto1.addEventListener("click", testRetoUno);
 
 const btnSuccses = succses.querySelector("button");
 btnSuccses.addEventListener("click", () => {
-   succses.setAttribute("hidden", true);
-   inicio.removeAttribute("hidden");
+  succses.setAttribute("hidden", true);
+  inicio.removeAttribute("hidden");
 });
 
 const btnWasted = wasted.querySelector("button");
 btnWasted.addEventListener("click", () => {
-   wasted.setAttribute("hidden", true);
-   inicio.removeAttribute("hidden");
+  wasted.setAttribute("hidden", true);
+  inicio.removeAttribute("hidden");
+});
+
+togglePista1.addEventListener("click", () => {
+  if (pista1.hasAttribute("hidden")) {
+    pista1.removeAttribute("hidden");
+  } else {
+    pista1.setAttribute("hidden", true);
+  }
 });
